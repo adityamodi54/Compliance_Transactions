aef2d92d1e35d81be14a035b9cfa9f1eca9b9edf
Aditya Modi <adityamodi54@gmail.com>
2024-06-16 23:35:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import re
 from datetime import datetime
+from kafka import KafkaProducer
+from kafka.errors import NoBrokersAvailable
 import json
 
 # Function to check email format using regular expressions
